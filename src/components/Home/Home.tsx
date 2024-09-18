7ed36a1e7d06e50ba950969c87d4ccf46a4b2d0d
GittySinai <gitty.sinai214@gmail.com>
2024-09-18 19:57:51 +0300

    Set up project with 'home' and 'weather-card' components and install Bootstrap

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React, { FC, useState } from 'react';
+import './Home.scss';
+
+interface HomeProps {}
+
+const Home  = () => {
+ 
+  
+  return <div className="Home">
+
+<h1>
+מזג האוויר בערים
+</h1>
+  </div>
+}
+
+export default Home;
---
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './WeatherCard.scss';
+
+interface WeatherCardProps {}
+
+const WeatherCard: FC<WeatherCardProps> = () => (
+  <div className="WeatherCard">
+    WeatherCard Component
+  </div>
+);
+
+export default WeatherCard;
